7a7b8d9f7ae3d29b477ffcb989798778e52179a3
Fumio Furukawa <fumio@jumilla.me>
2016-12-06 14:53:28 +0900

    Upgrade to Bootstrap 3.3.7 with JQuery 3.1.1

--- stubs/ui/views/layouts/app.blade.php
+++ stubs/ui/views/layouts/app.blade.php
@@ -6,11 +6,11 @@
 
 @section('styles')
 {{-- CSS指定のプレースホルダです。 --}}
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 @endsection
 
 @section('scripts')
 {{-- JavaScript指定のプレースホルダです。 --}}
-	<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 @endsection
